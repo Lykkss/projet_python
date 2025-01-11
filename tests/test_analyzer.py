56c094f80ec9f33c91484093362df6d7613203e0
3Mcrystal <50374471+3Mcrystal@users.noreply.github.com>
2025-01-11 18:28:36 +0100

    return the data instead of print it, add docstring for analyzer.py

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -1,13 +1,10 @@
 import os
 import sys
-
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 from analyzer import analyze_csv
 
 def test_analyze_csv():
-
-    # Check if the file exists before running the test
     file_path = "day_wise.csv"
     data_dir = 'data'
     full_file_path = os.path.join(data_dir, file_path)
@@ -16,10 +13,9 @@ def test_analyze_csv():
         print(f"Error: {full_file_path} does not exist. Please check the file path.")
         return
     
-    # Run the analysis on the day_wise.csv file
     print(f"Running analysis on {file_path}...\n")
-    analyze_csv(file_path)
+    summary = analyze_csv(file_path)
+    print(summary)
 
-# Execute the test
 if __name__ == "__main__":
     test_analyze_csv()
